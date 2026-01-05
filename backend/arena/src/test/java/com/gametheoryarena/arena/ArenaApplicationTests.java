4ce5049b21298c8f4de608dcf3fa168f1a67476b
Ramchander Venugopal <helloram30@gmail.com>
2026-01-05 03:06:31 -0700

    init commit

---
+++ backend/arena/src/test/java/com/gametheoryarena/arena/ArenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gametheoryarena.arena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArenaApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Ensures Spring context starts.
+    }
+}
